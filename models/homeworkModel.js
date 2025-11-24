427bf263e3a440ebf3d2e787c946771a711a36de
rachit367 <rachit367@gmail.com>
2025-11-24 19:44:00 +0530

    models completed

---
+++ models/homeworkModel.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+
+const homeworkSchema = new mongoose.Schema({
+    school_id:{ type: mongoose.Schema.Types.ObjectId, ref: "School",required:true },
+    created_by:{ type: mongoose.Schema.Types.ObjectId, ref: "User" ,required:true},
+    class_id:{ type: mongoose.Schema.Types.ObjectId, ref: "Class" ,required:true},
+    topic:String,
+    description:String,
+    attachments:[{
+        name:String,
+        url:String
+    }],
+    deadline:Date,
+    submitted_by:[{
+        student_id:{type:mongoose.Schema.Types.ObjectId,ref:'User'},
+        file_url:{type:String},
+        submitted_at:{type:Date,default:Date.now}
+    }]
+},{timestamps:true})
+
+module.exports = mongoose.model('Homework', homeworkSchema)
---
+++ models/ptmModel.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+
+const ptmSchema = new mongoose.Schema({
+    teacher_id: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    student_id: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    school_id: { type: mongoose.Schema.Types.ObjectId, ref: "School" },
+    date:{type:Date},
+    time:{type:String},
+    parent_name:String,
+    status:{
+        type:String,
+        enum:['accepted','done','rejected','pending'],
+        default:'pending'
+    },
+
+},{timestamps:true})
+
+module.exports = mongoose.model('Ptm', ptmSchema)
